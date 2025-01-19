36222acfc2f0b2f3f0a9c01e0cf3a04dc82eead1
Anatoliy <kim.anatoliy2007@gmail.com>
2025-01-19 18:47:05 +0500

    Added Get_Started 3rd page

---
+++ lab1_py/Get_Started.py
@@ -0,0 +1,6 @@
+#To check the Python version of the editor, you can find it by importing the sys module:
+import sys
+print(sys.version)
+
+""" To test a short amount of code in python sometimes it is quickest and easiest not to write the code in a file. This is made possible because Python can be run as a command line itself."""
+print ("Hello World!")
