8a2e4476ebdf767d809311ce1a42dfbed8742e31
Anatoliy <kim.anatoliy2007@gmail.com>
2025-02-12 11:58:43 +0500

    solved with generator

--- lab4/iterators_generators/1_gen.py
+++ lab4/iterators_generators/1_gen.py
@@ -1,15 +1,13 @@
-squares = [] #лист куда мы отправляем квадраты
 
 def square(n): #функция квадрата
     i=1
     while i < n:
-        squares.append(i**2)
+        yield i**2 #создание генератора
         i+=1
-
+    
 m = int(input())
-square(m)
 
-sq = iter(squares)
+for num in square(m): #вывод квадратов
+    print (num)
 
-for i in range (len(squares)): #функция вывода
-    print (next(sq))
+
