89a24d55c8a4628bf1a119c90e72de9d7fe9bb52
Anatoliy <kim.anatoliy2007@gmail.com>
2025-02-16 20:51:13 +0500

    math2

---
+++ lab4/math/2.py
@@ -0,0 +1,9 @@
+import math
+
+height = int(input("Add parameter: "))
+First = int(input("Add parameter: "))
+Second = int(input("Add parameter: "))
+
+area = 1/2 * (First+Second)* height
+
+print (area)
