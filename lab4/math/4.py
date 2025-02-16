83232aaa52e39a43772be036409cca9bb905215b
Anatoliy <kim.anatoliy2007@gmail.com>
2025-02-16 21:10:07 +0500

    math4

---
+++ lab4/math/4.py
@@ -0,0 +1,8 @@
+import math
+
+length_of_base = int(input("Введи основание: "))
+height_of_parallelogram = int(input("Введи высоту: "))
+
+area = length_of_base * height_of_parallelogram
+
+print (area)
